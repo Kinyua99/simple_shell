a24b021c54f6d7726ef24a2c475aeb1504a11e88
Kinyua99 <kinyuangumba01@gmail.com>
2023-02-22 10:57:45 +0300

    Edit aux_stdlib.c

--- aux_stdlib.c
+++ aux_stdlib.c
@@ -72,8 +72,7 @@ char *aux_itoa(int n)
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0;
-	//oi = 0, pn = 1, m = 1, i;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
 	while (*(s + count) != '\0')
 	{
@@ -81,5 +80,21 @@ int _atoi(char *s)
 			break;
 
 		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
 	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
